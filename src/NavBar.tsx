a4bc11115dc878bbafb26dee9db1e37ce3967f94
Christopher Owen <chris.owen@sigmalabs.co.uk>
2024-12-31 12:32:11 +0000

    Update NavBar.tsx

--- src/NavBar.tsx
+++ src/NavBar.tsx
@@ -1,3 +1,6 @@
+
+import { NavLink } from 'react-router-dom';
+
 function NavBar() {
     return (
         <nav className="navbar bg-primary navbar-expand-lg" data-bs-theme="dark" style={{ margin: '24px', padding: '12px', borderRadius: '8px' }}>
@@ -6,14 +9,20 @@ function NavBar() {
                     Code Review
                 </span>
                 <div className="collapse navbar-collapse">
-                    <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-                        <li className="nav-item">
-                            <a className="nav-link" href="/CodingCoach/review">Home</a>
-                        </li>
-                        <li className="nav-item">
-                            <a className="nav-link" href="/CodingCoach/repo">Import from Github</a>
-                        </li>
-                    </ul>
+                    <nav>
+                        <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+                            <li className="nav-item">
+                                <NavLink className="nav-link" to="/CodingCoach/review" end>
+                                    Home
+                                </NavLink>
+                            </li>
+                            <li className="nav-item">
+                                <NavLink className="nav-link" to="/CodingCoach/repo" end>
+                                    Import from Github
+                                </NavLink>
+                            </li>
+                        </ul>
+                    </nav>
                 </div>
             </div>
         </nav>
